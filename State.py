296c0a64c7449b163883158bf7f53baac7517061
Elysian Spirit <abhig0209@gmail.com>
2020-03-29 19:12:05 +0530

    requirements updated

--- State.py
+++
@@ -1,5 +0,0 @@
-final = ["Andaman & Nicobar", "Andhra Pradesh", "Arunachal Pradesh", "Assam", "Bihar", "Chandigarh", "Chhattisgarh", "Dadra & Nagar Haveli", "Daman & Diu", "Delhi", "Goa", "Gujarat", "Haryana", "Himachal Pradesh", "Jammu & Kashmir", "Jharkhand",
-         "Karnataka", "Kerala", "Lakshadweep", "Madhya Pradesh", "Maharashtra", "Manipur", "Meghalaya", "Mizoram", "Nagaland", "Orissa", "Pondicherry", "Punjab", "Rajasthan", "Sikkim", "Tamil Nadu", "Tripura", "Uttar Pradesh", "Uttaranchal", "West Bengal"]
-
-for state in final:
-    print(f"<option value='{state}'> {state} </option>")
